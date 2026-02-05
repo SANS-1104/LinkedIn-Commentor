bdbb5d980970972a2c12045414e1ce808757cc5f
Sanskriti Srivastava <sanskriti020803@gmail.com>
2026-02-05 13:24:27 +0530

    commit 7

--- api/generate-comments.js
+++ api/generate-comments.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
     }
 
     const GEMINI_API_URL =
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-pro:generateContent?key=" +
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=" +
       process.env.GEMINI_API_KEY;
 
     const prompt = `
